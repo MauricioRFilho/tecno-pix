e23508c278adcbe8e48acb78a3995ea9bfed4ee2
Mauricio Filho <mauricio.santos0198@gmail.com>
2026-03-31 16:57:42 -0300

    fix rota do swagger

--- config/autoload/swagger.php
+++ config/autoload/swagger.php
@@ -13,7 +13,61 @@ return [
     'enable' => true,
     'port' => 9500,
     'json_dir' => BASE_PATH . '/storage/swagger',
-    'html' => null,
+    'html' => <<<'HTML'
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="SwaggerUI" />
+    <title>Tecno Pix API Docs</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui.css" />
+    <style>
+      html {
+        box-sizing: border-box;
+        overflow-y: scroll;
+      }
+
+      *,
+      *::before,
+      *::after {
+        box-sizing: inherit;
+      }
+
+      body {
+        margin: 0;
+        background: #f4f7fb;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="swagger-ui"></div>
+    <script src="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui-bundle.js" crossorigin></script>
+    <script src="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui-standalone-preset.js" crossorigin></script>
+    <script>
+      window.onload = () => {
+        window.ui = SwaggerUIBundle({
+          url: getQueryString("search"),
+          dom_id: '#swagger-ui',
+          presets: [
+            SwaggerUIBundle.presets.apis,
+            SwaggerUIStandalonePreset
+          ],
+          layout: "StandaloneLayout",
+        });
+      };
+
+      function getQueryString(name) {
+        const reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+        const result = window.location.search.substr(1).match(reg);
+        const context = result !== null ? decodeURIComponent(result[2]) : "";
+
+        return context === "" || context === "undefined" ? "/http.json" : context;
+      }
+    </script>
+  </body>
+</html>
+HTML,
     'url' => '/swagger',
     'auto_generate' => true,
     'scan' => [
