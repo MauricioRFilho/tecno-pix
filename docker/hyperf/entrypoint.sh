68896f1af8a307564faae965f1cfd406b934d6e0
Mauricio Filho <mauricio.santos0198@gmail.com>
2026-03-31 14:36:32 -0300

    docker on

--- docker/hyperf/entrypoint.sh
+++ docker/hyperf/entrypoint.sh
@@ -13,5 +13,51 @@ if [ ! -d vendor ]; then
   composer install --no-interaction
 fi
 
-exec /usr/bin/supervisord -c /etc/supervisord.conf
+SUPERVISOR_CONFIG=/tmp/supervisord.generated.conf
 
+cat /etc/supervisord.conf > "$SUPERVISOR_CONFIG"
+
+cat <<'EOF' >> "$SUPERVISOR_CONFIG"
+
+[program:hyperf-http]
+command=php /var/www/html/bin/hyperf.php start
+directory=/var/www/html
+autostart=true
+autorestart=true
+stdout_logfile=/dev/stdout
+stdout_logfile_maxbytes=0
+stderr_logfile=/dev/stderr
+stderr_logfile_maxbytes=0
+EOF
+
+if php /var/www/html/bin/hyperf.php list | grep -q "queue:work"; then
+  cat <<'EOF' >> "$SUPERVISOR_CONFIG"
+
+[program:hyperf-queue]
+command=php /var/www/html/bin/hyperf.php queue:work
+directory=/var/www/html
+autostart=true
+autorestart=true
+stdout_logfile=/dev/stdout
+stdout_logfile_maxbytes=0
+stderr_logfile=/dev/stderr
+stderr_logfile_maxbytes=0
+EOF
+fi
+
+if php /var/www/html/bin/hyperf.php list | grep -q "crontab:run"; then
+  cat <<'EOF' >> "$SUPERVISOR_CONFIG"
+
+[program:hyperf-cron]
+command=php /var/www/html/bin/hyperf.php crontab:run
+directory=/var/www/html
+autostart=true
+autorestart=true
+stdout_logfile=/dev/stdout
+stdout_logfile_maxbytes=0
+stderr_logfile=/dev/stderr
+stderr_logfile_maxbytes=0
+EOF
+fi
+
+exec /usr/bin/supervisord -c "$SUPERVISOR_CONFIG"
